aaf4b5789eb9439c8ea44799005e1fde56a31e1e
Vikramjit Bora <vikramjit.bora@gmail.com>
2023-06-16 01:22:11 +0530

    Add functions.py

---
+++ functions.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import re
+
+
+def get_dataframe(file='data.csv'):
+    '''
+    This will read an existing dataframe from location mentioned in file. If
+    file path doesnt exist, it will create a dataframe with 'data' and 'labels'
+    columns
+    :param file: file path to read the dataframe if it exists
+    :return: Pandas Dataframe object
+    '''
+    try:
+        data = pd.read_csv('data.csv')
+    except FileNotFoundError:
+        data = pd.DataFrame(columns=['data', 'labels'])
+    return data
+
+
+def get_sentences(text_data, number_sentences):
+    '''
+    Function to get number of sentences from the data. If number entered is greater
+    than total number of sentences, it will return the entire paragraph
+    :param text_data: A paragraph of text data
+    :param number_sentences(int): Number of sentences to extract
+    :return: A string object with the required number of sentences
+    '''
+    sentences = re.split(r' *[।?!]', text_data)
+    sentences = [sentence.replace('\n', '') for sentence in sentences]
+    if len(sentences) < number_sentences:
+        return '।'.join(sentences)
+    else:
+        return '।'.join(sentences[:number_sentences])
