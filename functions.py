804e6d5bab63cbeb05a17b7b27f1beb7cdebdf81
Vikramjit Bora <vikramjit.bora@gmail.com>
2023-06-16 09:02:19 +0530

    Add refresh logic to app

--- functions.py
+++ functions.py
@@ -3,13 +3,13 @@ import re
 
 
 def get_dataframe(file='data.csv'):
-    '''
+    """
     This will read an existing dataframe from location mentioned in file. If
     file path doesnt exist, it will create a dataframe with 'data' and 'labels'
     columns
     :param file: file path to read the dataframe if it exists
     :return: Pandas Dataframe object
-    '''
+    """
     try:
         data = pd.read_csv('data.csv')
     except FileNotFoundError:
@@ -18,13 +18,13 @@ def get_dataframe(file='data.csv'):
 
 
 def get_sentences(text_data, number_sentences):
-    '''
+    """
     Function to get number of sentences from the data. If number entered is greater
     than total number of sentences, it will return the entire paragraph
     :param text_data: A paragraph of text data
-    :param number_sentences(int): Number of sentences to extract
+    :param number_sentences: Number of sentences to extract (Int)
     :return: A string object with the required number of sentences
-    '''
+    """
     sentences = re.split(r' *[।?!]', text_data)
     sentences = [sentence.replace('\n', '') for sentence in sentences]
     if len(sentences) < number_sentences:
--- web.py
+++ web.py
@@ -1,6 +1,8 @@
 import glob
+import os
 import pandas as pd
 import streamlit as st
+
 from functions import get_dataframe, get_sentences
 
 NUM_SENT = 3
@@ -12,23 +14,35 @@ df = get_dataframe()
 
 st.title('Domain Identification for NLP')
 
-
-for filepath in FILEPATHS:
+try:
+    filepath = FILEPATHS[0]
     with open(filepath, encoding='utf-8') as file:
         text = file.read()
         # Extract sentences from the text
         with st.form(filepath, clear_on_submit=True):
             display = get_sentences(text, NUM_SENT)
             st.text_area('Text to analyse', display)
+
             label = st.selectbox(
                 'Please select the right category for above text',
                 DOMAINS, index=0
             )
             if label != 'Select':
                 st.write('You selected: ', label)
+
             submitted = st.form_submit_button("Submit")
             if submitted:
-                new_row = {'data': [display], 'labels': [label]}
-                df = pd.concat([df, pd.DataFrame(new_row)], ignore_index=True)
+                if display in df['data'].values:
+                    df.loc[df.data == display, 'labels'] = label
+                else:
+                    new_row = {'data': [display], 'labels': [label]}
+                    df = pd.concat([df, pd.DataFrame(new_row)], ignore_index=True)
+    df.to_csv('data.csv', index=False)
 
-df.to_csv('data.csv', index=False)
+    next_button = st.button('Next')
+    if next_button:
+        if display in df['data'].values:
+            os.remove(filepath)
+        st.experimental_rerun()
+except IndexError:
+    st.write('Data files are not present in your location')
